42bf5dca04211325f29d9636a08f0efafea8f8ed
erdem.savar <erdem.savar@cash-on-ledger.com>
2022-02-03 22:10:16 +0100

    async await in nodejs

---
+++ business.js
@@ -0,0 +1,75 @@
+class Business
+{
+    constructor(){
+
+    }
+
+     async method1(callback){
+
+        return new Promise(async (resolve, reject) => {
+
+            try {
+
+                await new Promise(resolve => {
+
+                    setTimeout(() => {
+                        console.log('method1 first process');
+                        resolve();
+                    }, 10000);
+
+                });
+                
+                
+                console.log('method1 second process');
+                resolve("XXX");
+                
+            } catch (error) {
+
+                reject(error);
+                
+            }
+            
+
+        });
+
+        
+    }
+
+    async method2(callback){
+
+        setTimeout(() => {
+            console.log('method2 finished');
+           
+        }, 5000);
+        // callback();
+        console.log('method2 second process');
+    }
+
+    async method3(callback){
+
+        setTimeout(() => {
+            console.log('method3 finished');
+         
+        }, 1000);
+       // callback();
+       console.log('method3 second process');
+    }
+
+    method4(){
+        return new Promise((resolve,reject) => {
+
+            setTimeout(() => {
+            
+                resolve('xxx');
+             
+            }, 2000);
+
+        });
+        
+    }
+
+
+
+}
+
+module.exports = Business;
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const Business = require('./business');
+
+const api = express();
+
+api.use("/get", (req, res, next) => {
+
+    main();
+
+    res.json({
+        success : true
+    });
+
+});
+
+const main = async () => {
+    console.log('Process started');
+    const business = new Business();
+
+
+
+    const res = await business.method1();
+    console.log(res);
+
+
+    await business.method2();
+
+    await business.method3();
+
+    //const ret = await business.method4();
+    //console.log(ret);
+
+
+    console.log('Process end');
+}
+
+
+
+api.listen(2121, () => {
+
+    console.log("App started on 2121 port");
+
+});
